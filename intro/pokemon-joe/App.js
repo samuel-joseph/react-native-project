1283758b958d3fa24a905ad99781cf89d9fac595
arcanlte <pinangayjoel@gmail.com>
2020-03-02 17:37:22 +0800

    Emulated at mobile not in emulator.

---
+++ intro/pokemon-joe/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+
+export default function App() {
+  return (
+    <View style={}>
+      <View>
+        <TextInput />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({});
