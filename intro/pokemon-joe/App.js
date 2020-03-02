05ed09ffd95d87ba58a96e7c3f911631972738f5
arcanlte <pinangayjoel@gmail.com>
2020-03-02 18:04:00 +0800

    Styled the components.

--- intro/pokemon-joe/App.js
+++ intro/pokemon-joe/App.js
@@ -3,13 +3,28 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={}>
-      <View>
-        <TextInput />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
       <View></View>
     </View>
   );
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: 200,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  }
+});
