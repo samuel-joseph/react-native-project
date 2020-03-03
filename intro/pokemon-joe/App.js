c3637cbb414fbe1cdaee9d79a357d4214a498b9b
arcanlte <pinangayjoel@gmail.com>
2020-03-03 13:38:07 +0800

    GoalItem is set.

--- intro/pokemon-joe/App.js
+++ intro/pokemon-joe/App.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 
 import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
@@ -29,15 +30,11 @@ export default function App() {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
+      <GoalInput
+        goalInputHandler={goalInputHandler}
+        enteredGoal={enteredGoal}
+        addGoalHandler={addGoalHandler}
+      />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
@@ -49,16 +46,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  input: {
-    width: 200,
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10
   }
 });
---
+++ intro/pokemon-joe/components/GoalInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+import GoalItem from "./GoalItem";
+import { View, TextInput, StyleSheet, Button } from "react-native";
+
+const GoalInput = props => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Course Goal"
+        style={styles.input}
+        onChangeText={props.goalInputHandler}
+        value={props.enteredGoal}
+      />
+      <Button title="ADD" onPress={props.addGoalHandler} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: 200,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  }
+});
+
+export default GoalInput;
