b1e5029fa93b4db85159bb49652b8d46433b475e
arcanlte <pinangayjoel@gmail.com>
2020-03-04 21:48:19 +0800

    Buttons styled.

--- intro/guess-num/screen/StartGameScreen.js
+++ intro/guess-num/screen/StartGameScreen.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
+import Card from "../components/Card";
+
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} />
+          </View>
+          <View style={styles.button}>
+            <Button style={styles.button} title="Confirm" onPress={() => {}} />
+          </View>
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -30,21 +36,16 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: "80%",
-    alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    elevation: 5,
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10
+    alignItems: "center"
   },
   buttonContainer: {
     flexDirection: "row",
     width: "100%",
     justifyContent: "space-between",
     paddingHorizontal: 15
+  },
+  button: {
+    width: "45%"
   }
 });
 
