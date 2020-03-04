8f1064e61f6b240f6bed3486fb6b5c260e6b5c26
arcanlte <pinangayjoel@gmail.com>
2020-03-04 20:49:58 +0800

    Buttons added.

--- intro/guess-num/screen/StartGameScreen.js
+++ intro/guess-num/screen/StartGameScreen.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
 const StartGameScreen = props => {
   return (
-    <View>
-      
+    <View style={styles.screen}>
+      <Text style={styles.title}>The Game Screen!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Rest" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
- }
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {},
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15
+  }
+});
 
 export default StartGameScreen;
