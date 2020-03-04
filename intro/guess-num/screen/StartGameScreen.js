66a010310819d00a54f9114d6b024485c731bc99
arcanlte <pinangayjoel@gmail.com>
2020-03-04 23:18:36 +0800

    Applied hooks in StartGameScreen.js

--- intro/guess-num/screen/StartGameScreen.js
+++ intro/guess-num/screen/StartGameScreen.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
 
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Colors from "../constants/colors";
 const StartGameScreen = props => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const numberInputHandler = inputText => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <Input style={styles.input} />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+          onChangeText={numberInputHandler}
+          value={enteredValue}
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button title="Reset" onPress={() => {}} color={Colors.accent} />
@@ -54,7 +69,8 @@ const styles = StyleSheet.create({
     width: "45%"
   },
   input: {
-    width: 50
+    width: 50,
+    textAlign: "center"
   }
 });
 
