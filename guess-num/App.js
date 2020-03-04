c6a40bd043a68c5d3c86808e39d5ab12170449a9
arcanlte <pinangayjoel@gmail.com>
2020-03-05 01:07:02 +0800

    So far so good

--- guess-num/App.js
+++ guess-num/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+// 
+// YOU ARE AT 5:18:44 MARK
+// 
+
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 import Header from "./components/Header";
 import StartGameScreen from "./screen/StartGameScreen";
+import GameScreen from "./screen/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
--- guess-num/screen/GameScreen.js
+++ guess-num/screen/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -24,7 +24,7 @@ const GameScreen = props => {
     <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <Card>
+      <Card style={styles.buttonContainer}>
         <Button title="LOWER" onPress={() => {}} />
         <Button title="GREATER" onPress={() => {}} />
       </Card>
@@ -37,6 +37,13 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%"
   }
 });
 
