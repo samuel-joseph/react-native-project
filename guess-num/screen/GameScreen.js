6bd55f1fb7d270c2b28009c59f3df52326b1fcfa
arcanlte <pinangayjoel@gmail.com>
2020-03-05 00:15:37 +0800

    Removed unnecessary folders and files.

---
+++ guess-num/screen/GameScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const generateRandomBetween = (min, max, exclude) => { };
+
+const GameScreen = props => {};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
