bd1f9a54546d72f41b6f8cb4b2fd900f29cb04cf
arcanlte <pinangayjoel@gmail.com>
2020-03-04 20:26:58 +0800

    screen folder created, StartGameScreen.js component created.

--- intro/guess-num/App.js
+++ intro/guess-num/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1
+  }
 });
