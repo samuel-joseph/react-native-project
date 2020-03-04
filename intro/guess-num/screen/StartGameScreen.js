53a47755b40b63d1ac5c031423f98d0489b44d64
arcanlte <pinangayjoel@gmail.com>
2020-03-04 20:31:04 +0800

    StartGameScreen initiated.

---
+++ intro/guess-num/screen/StartGameScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const StartGameScreen = props => {
+  return (
+    <View>
+      
+    </View>
+  );
+ }
+
+const styles = StyleSheet.create({})
+
+export default StartGameScreen;
