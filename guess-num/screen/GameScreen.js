feb293c789a62b530beba844e509a100c194544c
arcanlte <pinangayjoel@gmail.com>
2020-03-05 00:36:24 +0800

    Styling for GameScreen.js is initiated.

--- guess-num/screen/GameScreen.js
+++ guess-num/screen/GameScreen.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -9,13 +12,32 @@ const generateRandomBetween = (min, max, exclude) => {
     return generateRandomBetween(min, max, exclude);
   } else {
     return rndNum;
-  } 
+  }
 };
 
 const GameScreen = props => {
-  const [currentGuess, setCurrentGuess] = useState(generateRandomBetween());
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  }
+});
 
 export default GameScreen;
